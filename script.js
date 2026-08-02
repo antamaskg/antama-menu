9da161edc6b943a2e1702598152aecc173cc8430
antamaskg <a.tsamourtzoglou@gmail.com>
2026-08-02 22:48:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+fetch("menu.json")
+  .then(response => response.json())
+  .then(menu => {
 
+    const products = document.getElementById("products");
+
+    menu["Ορεκτικά"].forEach(item => {
+
+      products.innerHTML += `
+        <div class="card">
+          <img src="${item.image}" alt="${item.name}">
+          <div class="card-body">
+            <h3>${item.name}</h3>
+            <span class="price">${item.price}</span>
+          </div>
+        </div>
+      `;
+
+    });
+
+  });
